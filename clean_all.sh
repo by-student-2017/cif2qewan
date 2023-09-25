53696870b64209a82b5fe5e113af5bd1ed307481
by-student-2017 <studentsctest@gmail.com>
2023-09-25 17:24:46 +0900

    ubuntu 22.04 LTS

---
+++ clean_all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm -f cif_scf.in
+
+rm -f scf.in scf.out
+rm -f nscf.in nscf.out
+
+rm -f pw2wan.in pw2wan.out
+rm -f proj.in
+rm -f pwscf.win pwscf.wout
+
+rm -f pwscf.amn pwscf.mmn pwscf.eig
+
+rm -f pwscf.nnkp pwscf.chk
+
+rm -f pwscf_band.gnu pwscf_band.dat pwscf_band.labelinfo.dat
+rm -f pwscf_band.kpt
+
+rm -f pwscf.bxsf
+
+rm -f pwscf_hr.dat pwscf_tb.dat pwscf_wsvec.dat
+
+rm -f -r band
+rm -f -r work_nscf
+rm -f -r check_wannier
